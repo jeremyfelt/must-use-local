8a02515e33521c544c9b3b59da424ddcf84f3e67
jeremyfelt <jeremy.felt@gmail.com>
2021-02-25 11:02:46 -0800

    Fix the function name 🤦🏻

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@ add_action( 'phpmailer_init', __NAMESPACE__ . '\phpmailer_init_mailhog' );
  *
  * @param \PHPMailer\PHPMailer\PHPMailer $phpmailer The PHPMailer instance.
  */
-function mu_local_phpmailer_init( $phpmailer ) {
+function phpmailer_init_mailhog( $phpmailer ) {
 	$phpmailer->IsSMTP();
 	$phpmailer->Host = '127.0.0.1';
 	$phpmailer->Port = 1025;
