9b1da2c6d5518ccfc2f39483eddf0eaa3d053810
jeremyfelt <jeremy.felt@gmail.com>
2021-02-26 18:18:07 -0800

    Add an example mu-autoloader.php file

---
+++ mu-autoloader.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Load all mu-plugins within subfolders where a PHP file exists with
+ * the same name as the directory name.
+ *
+ *  Example: /must-use-local/must-use-local.php
+ *
+ * If you're looking at this file in the Must Use Local mu-plugin, and need
+ * something like it, copy it to your `wp-content/mu-plugins/` directory.
+ */
+$dirs = glob( dirname( __FILE__ ) . '/*' , GLOB_ONLYDIR );
+foreach( $dirs as $dir ) {
+    if( file_exists( $dir . DIRECTORY_SEPARATOR . basename( $dir ) . '.php' ) ) {
+        require( $dir . DIRECTORY_SEPARATOR . basename( $dir ) . '.php' );
+    }
+}
